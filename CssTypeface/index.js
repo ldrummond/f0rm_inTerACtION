a7781c0c379e58d27e578ad69e6ae76ea76fcb96
Lucas Drummond <ldrummond@wustl.edu>
2018-08-29 09:50:03 -0500

    setting up CssTypeface route

---
+++ CssTypeface/index.js
@@ -0,0 +1,138 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.sass'
+
+const characterSet = {
+  "A": {
+    path : "",
+  },
+  "B": {
+    path : "",
+  },
+  "C": {
+    path : "",
+  },
+  "D": {
+    path : "",
+  },
+  "E": {
+    path : "",
+  },
+  "F": {
+    path : "",
+  },
+  "G": {
+    path : "",
+  },
+  "H": {
+    path : "",
+  },
+  "I": {
+    path : "",
+  },
+  "K": {
+    path : "",
+  },
+  "L": {
+    path : "",
+  },
+  "M": {
+    path : "",
+  },
+  "N": {
+    path : "",
+  },
+  "O": {
+    path : "",
+  },
+  "P": {
+    path : "",
+  },
+  "Q": {
+    path : "",
+  },
+  "R": {
+    path : "",
+  },
+  "S": {
+    path : "",
+  },
+  "T": {
+    path : "",
+  },
+  "V": {
+    path : "",
+  },
+  "X": {
+    path : "",
+  },
+  "Y": {
+    path : "",
+  },
+  "Z": {
+    path : "",
+  },
+  "0": {
+    path : "",
+  },
+  "1": {
+    path : "",
+  },
+  "2": {
+    path : "",
+  },
+  "3": {
+    path : "",
+  },
+  "4": {
+    path : "",
+  },
+  "5": {
+    path : "",
+  },
+  "6": {
+    path : "",
+  },
+  "7": {
+    path : "",
+  },
+  "8": {
+    path : "",
+  },
+  "9": {
+    path : "",
+  },
+}
+
+export class App extends React.Component {
+  constructor() {
+    super()
+
+    let characterMarkup = []
+    for(const key in characterMarkup) {
+      const character = characterMarkup[key]
+      const path = character.path
+      let newCharacter = (
+        <div className="character">
+        {character}
+        </div>
+      )
+      characterMarkup.push(newCharacter)
+    }
+  }  
+
+  render() {
+    return(
+      <div className='site-wrapper'>
+        <div className='site-inner'>
+          {characterMarkup}
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
